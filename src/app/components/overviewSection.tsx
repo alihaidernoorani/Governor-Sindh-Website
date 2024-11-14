cc7307b8b55f14d923f4cd8bef58bb259d64466b
alihaidernoorani <alihaidernoorani@yahoo.com>
2024-11-14 17:32:44 +0500

    first commit

--- src/app/components/overviewSection.tsx
+++ src/app/components/overviewSection.tsx
@@ -12,9 +12,9 @@ export default function overviewSection() {
           The pace of technological change is accelerating. Big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.
         </p>
         <div className="flex flex-col justify-around gap-3 lg:flex-row mx-2 md:mx-32">
-          <Card src="/images/money.jpg" alt="Representation of financial growth" />
-          <Card src="/images/solopreneur.jpg" alt="Solopreneurship concept" />
-          <Card src="/images/IT class.jpg" alt="IT class in session" />
+          <Card src="/images/money.png" alt="Representation of financial growth" />
+          <Card src="/images/solopreneur.png" alt="Solopreneurship concept" />
+          <Card src="/images/IT class.png" alt="IT class in session" />
         </div>
       </section>
     )
